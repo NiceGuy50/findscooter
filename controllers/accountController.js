f36eb2ed4c2037c0569b2c22182035858a46d5a8
NiceGuy50 <benhaham@hotmail.com>
2024-02-25 17:15:15 +0200

    some update

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -4,8 +4,70 @@ import bcryptjs from "bcryptjs";
 import jsonwebtoken from "jsonwebtoken";
 import Account from "../models/accountModel.js";
 
-export default router;
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - firstName
+ *        - lastName
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: the auto-generated id of the user
+ *        firstName:
+ *          type: string
+ *          description: the first name of the user
+ *        lastName:
+ *          type: string
+ *          description: the last name of the user
+ *        email:
+ *          type: string
+ *          description: the email of the user
+ *        password:
+ *          type: string
+ *          description: the user password
+ *        verificationCode:
+ *          type: integer
+ *          description: Generated code for account validation
+ *        isVerified:
+ *          type: boolean
+ *          description: Get default value of false until user validation
+ *      example:
+ *       id: 1
+ *       firstName: Ben
+ *       lastName: Haham
+ *       email: benhaham@hotmail.com
+ *       password: password
+ *       verificationCode: 1234
+ *       isVerified: false
+ */
 
+/**
+ * @swagger
+ * /api/account/signup:
+ *  post:
+ *    summary: Create New User Account
+ *    tags: [Accounts]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: the user account was successfully created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *
+ */
 router.post("/signup", (req, res) => {
   // Get the user credentials
   const { firstName, lastName, email, password } = req.body;
@@ -49,8 +111,78 @@ router.post("/signup", (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * tags:
+ *  name: Accounts
+ *  description: The accounts managing API
+ */
+
+/**
+ * @swagger
+ * /api/account/users:
+ *  get:
+ *    summary: Return the list of all users
+ *    tags: [Accounts]
+ *    responses:
+ *      200:
+ *        description: The list of the users
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/User'
+ */
+
+router.get("/users", (req, res) => {
+  Account.findAll()
+    .then(results => {
+      return res.status(200).json({ message: results });
+    })
+    .catch(error => {
+      return res.status(500).json({ message: error });
+    });
+});
+
+/**
+ * @swagger
+ * /api/account/deleteAccount/{id}:
+ *  delete:
+ *    summary: delete an account by id
+ *    tags: [Accounts]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: the user account was deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ */
+router.delete("/deleteAccount/:id", (req, res) => {
+  const userId = req.params.id;
+  Account.findByPk(userId)
+    .then(results => {
+      return results.destroy().then(account => {
+        return res.status(200).json({
+          message: account,
+        });
+      });
+    })
+    .catch(error => {
+      return res.status(500).json({ message: error });
+    });
+});
+
 router.post("/verify", (req, res) => {});
 
 router.post("/login", (req, res) => {});
 
-router.get("/getAccount", (req, res) => {});
+export default router;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import accountController from "./controllers/accountController.js";
 import database from "./services/database.js";
 
@@ -7,8 +9,22 @@ dotenv.config();
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "FindScooter API Documentation",
+      version: "1.0.0",
+      description: "this is my first Swagger documentation",
+    },
+  },
+  apis: ["./controllers/accountController.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use("/api/account", accountController);
 
@@ -17,7 +33,7 @@ const port = process.env.PORT;
 database
   .sync()
   .then(results => {
-    console.log(results);
+    // console.log(results);
     app.listen(port, () => console.log(`server is running via port ${port}`));
   })
   .catch(error => console.log(error));
